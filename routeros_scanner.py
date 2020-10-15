9f2e80e741093112d6534b701149d060f794e9ec
Vasiliy N. Polikarpov <vasiliy.polikarpov@pimpay.ru>
2020-10-15 12:09:31 +0300

    Fix issue where routeros scanner doesn't try to reconnect api

--- routeros_scanner.py
+++ routeros_scanner.py
@@ -1,4 +1,5 @@
 import routeros_api
+import routeros_api.exceptions
 from datetime import datetime
 from ipaddress import ip_address, ip_network
 
@@ -9,18 +10,42 @@ class RouterOsScanner(AbstractScanner):
     def __init__(self, interval, address, username, password, ssl_context=None, subnet_filters=None):
         super().__init__(interval)
 
-        connection = routeros_api.RouterOsApiPool(
-            address, username=username, password=password, use_ssl=True, ssl_context=ssl_context, plaintext_login=True
-        )
-        self.api = connection.get_api()
+        self.api = None
+        self._address = address
+        self._username = username
+        self._password = password
+        self._ssl_context = ssl_context
+        self.connect_api()
 
         if not subnet_filters:
             self.subnet_filters = None
         else:
             self.subnet_filters = [ip_network(sf) for sf in subnet_filters]
 
+    def connect_api(self):
+        connection = routeros_api.RouterOsApiPool(
+            host=self._address,
+            username=self._username,
+            password=self._password,
+            use_ssl=True,
+            ssl_context=self._ssl_context,
+            plaintext_login=True,
+        )
+        self.api = connection.get_api()
+
     def scan(self):
-        hosts = self.api.get_resource('/ip/arp').get(complete='true')
+        if self.api is None:
+            self.connect_api()
+
+        for attempt in range(3):
+            try:
+                hosts = self.api.get_resource('/ip/arp').get(complete='true')
+                break
+            except routeros_api.exceptions.RouterOsApiConnectionError:
+                self.connect_api()
+        else:
+            raise RuntimeError("Can't use established api connection")
+
         self.last_scan = timestamp = datetime.now()
         for host in hosts:
             if self.subnet_filters:
